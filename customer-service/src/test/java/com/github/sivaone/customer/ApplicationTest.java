6635768ab2a7deb13b88da93e1c3524aac928260
Siva Nagi Reddy Alla <Sivanagireddy_Alla@epam.com>
2022-10-18 08:21:13 +0530

    Add dependencies

---
+++ customer-service/src/test/java/com/github/sivaone/customer/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.github.sivaone.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
